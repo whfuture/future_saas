e06f77f992767f4b822b13fdd654cc5fdbfa8c3b
whfuture <dengc0715@gmail.com>
2025-02-20 16:24:25 +0800

    add: util

--- future_base/future_common/src/test/java/wh/future/framework/common/AppTest.java
+++ future_base/future_common/src/test/java/wh/future/framework/common/AppTest.java
@@ -3,11 +3,15 @@ package wh.future.framework.common;
 
 import com.alibaba.fastjson.JSON;
 import org.junit.jupiter.api.Test;
+import wh.future.framework.common.enums.DateIntervalEnum;
 import wh.future.framework.common.enums.ErrorCodeConstants;
 import wh.future.framework.common.exception.AssertUtils;
+import wh.future.framework.common.util.date.TimeUtils;
 
 import java.text.MessageFormat;
+import java.time.LocalDateTime;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -25,6 +29,19 @@ public class AppTest {
         enums.add("A");
         System.out.println(MessageFormat.format("woshi {0}", JSON.toJSONString(enums)));
         AssertUtils.nonNull(null, ErrorCodeConstants.Test,"n ggggg","ssss");
+    }
+
+    @Test
+    public void test2(){
+        String s = TimeUtils.formatDateRange(LocalDateTime.now(), DateIntervalEnum.MONTH);
+        System.out.println(s);
+        LocalDateTime year = TimeUtils.getFirstDayOfYear();
+        System.out.println(year);
+        System.out.println(TimeUtils.getFirstDayOfMonth());
+        List<LocalDateTime[]> dateRangeList = TimeUtils.getDateRangeList(year, LocalDateTime.now(), DateIntervalEnum.MONTH);
+        for (LocalDateTime[] localDateTimes : dateRangeList) {
+            System.out.println(JSON.toJSONString(localDateTimes));
+        }
     }
 
 }
