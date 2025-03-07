f669b0138f5fc7fff2fcabc6e992f4166caebd92
whfuture <dengc0715@gmail.com>
2025-03-07 22:44:07 +0800

    add: 2025年3月7日 add api

---
+++ future_api/src/main/java/wh/future/framework/api/internal/logger/ApiAccessLogApi.java
@@ -0,0 +1,34 @@
+package wh.future.framework.api.internal.logger;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import wh.future.framework.api.internal.logger.dto.ApiAccessLogCreateReq;
+import wh.future.framework.api.internal.logger.enums.ApiConstants;
+import wh.future.framework.common.pojo.R;
+
+import javax.validation.Valid;
+
+/**
+ * @author Administrator
+ */
+@FeignClient(name = ApiConstants.NAME)
+@Tag(name = "RPC 服务 - API 访问日志")
+public interface ApiAccessLogApi {
+
+    String PREFIX = ApiConstants.PREFIX + "/api-access-log";
+
+    @PostMapping(PREFIX + "/create")
+    @Operation(summary = "创建 API 访问日志")
+    R<Boolean> createApiAccessLog(@Valid @RequestBody ApiAccessLogCreateReq req);
+
+    @Async
+    default void createApiAccessLogAsync(ApiAccessLogCreateReq req) {
+        createApiAccessLog(req);
+    }
+
+}
---
+++ future_api/src/main/java/wh/future/framework/api/internal/logger/ApiErrorLogApi.java
@@ -0,0 +1,30 @@
+package wh.future.framework.api.internal.logger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import wh.future.framework.api.internal.logger.dto.ApiErrorLogCreateReq;
+import wh.future.framework.api.internal.logger.enums.ApiConstants;
+import wh.future.framework.common.pojo.R;
+
+import javax.validation.Valid;
+
+@FeignClient(name = ApiConstants.NAME)
+@Tag(name = "RPC 服务 - API 异常日志")
+public interface ApiErrorLogApi {
+
+    String PREFIX = ApiConstants.PREFIX + "/api-error-log";
+
+    @PostMapping(PREFIX + "/create")
+    @Operation(summary = "创建 API 异常日志")
+    R<Boolean> createApiErrorLog(@Valid @RequestBody ApiErrorLogCreateReq createDTO);
+
+    @Async
+    default void createApiErrorLogAsync(ApiErrorLogCreateReq createDTO) {
+        createApiErrorLog(createDTO);
+    }
+
+}
