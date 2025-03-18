c4f73910b18283729679662152db9fd20ac18176
dengc <m17621679833@163.com>
2025-03-18 12:27:19 +0800

    add: 调整pom文件配置

--- future-framework/future-framework-security/src/main/java/wh/future/framework/security/business_log/conf/BusinessLogConfiguration.java
+++ future-framework/future-framework-security/src/main/java/wh/future/framework/security/business_log/conf/BusinessLogConfiguration.java
@@ -2,9 +2,11 @@ package wh.future.framework.security.business_log.conf;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import wh.future.framework.api.internal.business_logger.BusinessLogApi;
 
 @AutoConfiguration
 @Slf4j
-//@EnableFeignClients(clients = {OperateLogApi.class}) // 主要是引入相关的 API 服务
+@EnableFeignClients(clients = {BusinessLogApi.class}) // 主要是引入相关的 API 服务
 public class BusinessLogConfiguration {
 }
